ab8c10c52586665e587a1068c2b60f6d7b19c608
Your Name In Single Quotes <your email here in single quotes>
2020-07-19 23:46:44 +0300

    display three random products

--- js/app.js
+++ js/app.js
@@ -6,11 +6,18 @@
 </section>
 */
 var votes = document.getElementById('votes')
-var resultsWordDisplay = document.createElement ('strong')
-votes.appendChild (resultsWordDisplay)
+var resultsWordDisplay = document.createElement('strong')
+votes.appendChild(resultsWordDisplay)
 resultsWordDisplay.textContent = 'Results: '
 
-var list = document.createElement('li')
+/*for (let index = 1; index <= collections.length; index++) {
+    var list = document.createElement('li')
+votes.appendChild(list)
+list.textContent ='Image ' + index +': '+ collections.numberOfClicks + 
+    console.log(collections.numberOfClicks)
+}
+*/
+/*var list = document.createElement('li')
 votes.appendChild(list)
 
 var imageWordDisplay = document.createElement('p')
@@ -24,3 +31,120 @@ clicksNoDisplay.textContent = '5'
 var votesWordDisplay = document.createElement('p')
 list.appendChild(votesWordDisplay)
 votesWordDisplay.textContent = " Votes"
+
+//End of Results section Rendering*/
+
+
+
+
+
+
+
+
+
+
+
+//constructor:
+var collections = []
+var productsName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass']
+function Product(name, path) {
+    this.name = name
+    this.path = path
+
+    this.numberOfClicks = 0;
+    this.numberOfTimesShown = 0;
+}
+
+for (let index = 0; index < productsName.length; index++) {
+
+    var productObject = new Product(productsName[index], `/images/${productsName[index]}.jpg`)
+    collections.push(productObject)
+
+}
+
+function oneRandomNo() {
+    //random number from zero to 20 (length of collection)
+    var randomNo = Math.floor(Math.random() * collections.length)
+    return randomNo
+
+}
+var leftIndex = oneRandomNo ()
+var midIndex = oneRandomNo()
+var rightIndex = oneRandomNo()
+
+function oneRandomPath() {
+    //random number from zero to 20 (length of collection)
+    var randomNo = Math.floor(Math.random() * collections.length)
+    //random path
+    return collections[randomNo].path
+
+}
+
+function randomImg() {
+ for (let index = 0; index < 25; index++) {
+    var leftImgsrc = oneRandomPath ()
+    var midImgsrc = oneRandomPath()
+    var rightImgsrc = oneRandomPath()
+
+ }
+
+
+ while (leftImgsrc === midImgsrc || leftImgsrc === rightImgsrc || midImgsrc === rightImgsrc || leftImgsrc === midImgsrc ===rightImg ) {
+    leftImgsrc = oneRandomPath ()
+    midImgsrc = oneRandomPath()
+    rightImgsrc = oneRandomPath()
+ } 
+  //images section rendering:
+var leftImg = document.getElementById('leftImg')
+var midImg = document.getElementById('midImg')
+var rightImg = document.getElementById('rightImg')
+
+// count the number of times these were shown
+collections[leftIndex].numberOfTimesShown += 1;
+collections[midIndex].numberOfTimesShown += 1;
+collections[rightIndex].numberOfTimesShown += 1;
+
+ leftImg.setAttribute('src', leftImgsrc)
+ midImg.setAttribute('src', midImgsrc)
+ rightImg.setAttribute('src',rightImgsrc)
+}
+randomImg()
+/*
+
+imgSection.addEventListener('click',click)
+var totalClicks = 0
+function click(){
+    if (totalClicks < 25 ){
+        var clickedElement = event.target;
+        var clickedElementId = clickedElement.id;
+
+      if(clickedElementId === 'leftImg' || clickedElementId === 'midImg' || clickedElementId === 'rightImg' ){
+            totalClicks +=1;
+
+            if(clickedElementId === 'leftImg'){
+                collections[leftIndex].numberOfClicks +=1;
+            }
+            if(clickedElementId === 'midtImg'){
+                allGoats[midIndex].numberOfClicks +=1;
+            }
+
+            if(clickedElementId === 'leftImg'){
+                allGoats[rightIndex].numberOfClicks +=1;
+            }
+
+            randomImg();
+         } else {
+       // generateUserMessage();
+        imgSection.removeEventListener('click', goatClickHandler);
+    }
+}}
+
+function generateUserMessage(){   
+    for (let index = 1; index <= collections.length; index++) {
+        var list = document.createElement('li')
+    votes.appendChild(list)
+    list.textContent ='Image ' + index +': '+ collections[index].numberOfTimesShown  
+    } 
+    
+
+} generateUserMessage()*/
