f740bed689679f4bc8085cfa9fc63d1b8ceec4ac
Your Name In Single Quotes <your email here in single quotes>
2020-07-20 12:03:53 +0300

    resubmit the changes

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ function oneRandomNo() {
     return randomNo
 
 }
-var leftIndex = oneRandomNo ()
+var leftIndex = oneRandomNo()
 var midIndex = oneRandomNo()
 var rightIndex = oneRandomNo()
 
@@ -41,32 +41,37 @@ function oneRandomPath() {
 }
 
 function randomImg() {
- for (let index = 0; index < 25; index++) {
-    var leftImgsrc = oneRandomPath ()
-    var midImgsrc = oneRandomPath()
-    var rightImgsrc = oneRandomPath()
+    
+        var leftImgsrc = oneRandomPath()
+        var midImgsrc = oneRandomPath()
+        var rightImgsrc = oneRandomPath()
 
- }
+    
 
 
- while (leftImgsrc === midImgsrc || leftImgsrc === rightImgsrc || midImgsrc === rightImgsrc || leftImgsrc === midImgsrc ===rightImg ) {
-    leftImgsrc = oneRandomPath ()
-    midImgsrc = oneRandomPath()
-    rightImgsrc = oneRandomPath()
- } 
-  //images section rendering:
-var leftImg = document.getElementById('leftImg')
-var midImg = document.getElementById('midImg')
-var rightImg = document.getElementById('rightImg')
+    while (leftImgsrc === midImgsrc || leftImgsrc === rightImgsrc || midImgsrc === rightImgsrc || leftImgsrc === midImgsrc === rightImg) {
+        leftImgsrc = oneRandomPath()
+        midImgsrc = oneRandomPath()
+        rightImgsrc = oneRandomPath()
+    }
+    //images section rendering:
+    var leftImg = document.getElementById('leftImg')
+    var midImg = document.getElementById('midImg')
+    var rightImg = document.getElementById('rightImg')
+    while (leftIndex === midIndex || leftIndex === rightIndex || midIndex === rightIndex || leftIndex === midIndex === rightIndex) {
+        leftIndex = oneRandomPath()
+        midIndex = oneRandomPath()
+        rightIndex = oneRandomPath()
+    }
 
-// count the number of times these were shown
-collections[leftIndex].numberOfTimesShown += 1;
-collections[midIndex].numberOfTimesShown += 1;
-collections[rightIndex].numberOfTimesShown += 1;
+    // count the number of times these were shown
+    collections[leftIndex].numberOfTimesShown += 1;
+    collections[midIndex].numberOfTimesShown += 1;
+    collections[rightIndex].numberOfTimesShown += 1;
 
- leftImg.setAttribute('src', leftImgsrc)
- midImg.setAttribute('src', midImgsrc)
- rightImg.setAttribute('src',rightImgsrc)
+    leftImg.setAttribute('src', leftImgsrc)
+    midImg.setAttribute('src', midImgsrc)
+    rightImg.setAttribute('src', rightImgsrc)
 }
 randomImg()
 
@@ -75,41 +80,43 @@ imgSection.addEventListener('click', clickFnc);
 
 
 var totalClicks = 0
-function clickFnc(){
-    if (totalClicks < 25 ){
+function clickFnc() {
+    if (totalClicks < 25) {
 
         var clickedElement = event.target;
         var clickedElementId = clickedElement.id;
 
-      if(clickedElementId === 'leftImg' || clickedElementId === 'midImg' || clickedElementId === 'rightImg' ){
-            totalClicks +=1;
+        if (clickedElementId === 'leftImg' || clickedElementId === 'midImg' || clickedElementId === 'rightImg') {
+            totalClicks += 1;
+        }
 
-            if(clickedElementId === 'leftImg'){
-                collections[leftIndex].numberOfClicks +=1;
+            if (clickedElementId === 'leftImg') {
+                collections[leftIndex].numberOfClicks += 1;
             }
-            if(clickedElementId === 'midtImg'){
-                collections[midIndex].numberOfClicks +=1;
+            if (clickedElementId === 'midtImg') {
+                collections[midIndex].numberOfClicks += 1;
             }
 
-            if(clickedElementId === 'leftImg'){
-                collections[rightIndex].numberOfClicks +=1;
-                console.log(                collections[rightIndex].numberOfClicks +=1)
+            if (clickedElementId === 'rightImg') {
+                collections[rightIndex].numberOfClicks += 1;
+                console.log(collections[rightIndex].numberOfClicks += 1)
             }
 
             randomImg();
-         } else {
-       generateUserMessage();
-        imgSection.removeEventListener('click', clickFnc);
+        } else {
+            generateUserMessage();
+            imgSection.removeEventListener('click', clickFnc);
+        }
     }
-}}
 
-function generateUserMessage(){   
-    for (let index = 1; index <= collections.length; index++) {
+function generateUserMessage() {
+    for (let index = 0; index < collections.length; index++) {
         var list = document.createElement('li')
-    votes.appendChild(list)
+        votes.appendChild(list)
 
-    list.textContent = collections[index].name + ' had ' + collections[index].numberOfClicks  + ' votes and was shown ' +  collections[index].numberOfTimesShown + ' times ' 
-    } 
-    
+        list.textContent = collections[index].name + ' had ' + collections[index].numberOfClicks + ' votes and was shown ' + collections[index].numberOfTimesShown + ' times '
+    }
 
-} generateUserMessage()
+
+} 
+//comment
